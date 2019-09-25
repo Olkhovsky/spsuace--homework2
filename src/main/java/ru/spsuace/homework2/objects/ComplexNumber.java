2eb15cbfb5be6c12f124090892824783939c975c
olkhovsky <olkhovsky11@mail.ru>
2019-09-25 13:38:37 +0300

    Сделал ComplexNumber

--- src/main/java/ru/spsuace/homework2/objects/ComplexNumber.java
+++ src/main/java/ru/spsuace/homework2/objects/ComplexNumber.java
@@ -5,4 +5,47 @@ package ru.spsuace.homework2.objects;
  * Так же дополните класс методами, чтобы эти объекты можно было использовать в HashMap
  */
 public class ComplexNumber {
+    private final  double realPart;
+    private final double complexPart;
+
+    public ComplexNumber(Double realPart, Double complexPart) {
+        this.realPart = realPart;
+        this.complexPart = complexPart;
+    }
+
+    public double getRalPart() {
+        return realPart;
+    }
+
+
+    public double getComplexPart() {
+        return complexPart;
+    }
+
+    @Override
+    public String toString() {
+        char sign;
+        if (complexPart > 0) {
+            sign = '+';
+        } else {
+            sign = '-';
+        }
+        return "" + realPart + sign + Math.abs(complexPart) + 'i';
+    }
+
+    @Override
+    public int hashCode() {
+        String s = toString();
+        return  s.hashCode();
+    }
+
+    public boolean equals(Object o) {
+        if (o instanceof ComplexNumber) {
+            ComplexNumber number = (ComplexNumber)o;
+            if ((number.getRalPart() == realPart) && (number.getComplexPart() == complexPart)) {
+                return  true;
+            }
+        }
+        return  false;
+    }
 }
