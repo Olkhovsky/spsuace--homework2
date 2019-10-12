16868ba1320e88bfe816e3ae9679aa0c8a240e14
Olkhovsky <olkhovsky11a@mail.ru>
2019-10-12 11:35:21 +0300

    StringTasks

--- src/main/java/ru/spsuace/homework2/objects/october2/StringTasks.java
+++ src/main/java/ru/spsuace/homework2/objects/october2/StringTasks.java
@@ -1,5 +1,10 @@
 package ru.spsuace.homework2.objects.october2;
 
+
+import java.text.NumberFormat;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringTasks {
 
     /**
@@ -13,7 +18,57 @@ public class StringTasks {
      * У класса Character есть полезные методы, например Character.isDigit()
      */
     public static Number simpleValueOf(String str) {
-        return null;
+        if (str == null) {
+            return  null;
+        }
+        String result = "";
+        Pattern pattern  = Pattern.compile("\\d|e|-|\\.");
+        Matcher matcher = pattern.matcher(str);
+        while (matcher.find()) {
+            result += str.substring(matcher.start(), matcher.end());
+        }
+
+        pattern = Pattern.compile("e");
+        int count = GetCount(pattern, str);
+        if (count > 1) {
+            return null;
+        }
+        pattern = Pattern.compile("\\.");
+        count = GetCount(pattern,str);
+        if (count > 1) {
+            return null;
+        }
+
+        pattern = Pattern.compile("-");
+        count = GetCount(pattern,str);
+        if (count > 1) {
+            if (result.toCharArray()[0] == '-' && result.toCharArray()[1] == '-') {
+                return  null;
+            }
+        }
+
+        try {
+            return Integer.valueOf(result);
+        } catch (NumberFormatException e1) {
+            try {
+                return Long.valueOf(result);
+            } catch (NumberFormatException e2) {
+                try {
+                    return Double.valueOf(result);
+                } catch (NumberFormatException e3) {
+                    return  null;
+                }
+            }
+        }
+    }
+
+    private static int GetCount(Pattern pattern, String str) {
+        int count = 0;
+        Matcher matcher = pattern.matcher(str);
+        while (matcher.find()) {
+            count++;
+        }
+        return  count;
     }
 
 
@@ -21,6 +76,40 @@ public class StringTasks {
      * Тоже самое, что и выше, но нельзя пользоваться функциями valueOf()
      */
     public static Number valueOf(String str) {
-        return null;
+        if (str == null) {
+            return  null;
+        }
+        String result = "";
+        Pattern pattern  = Pattern.compile("\\d|e|-|\\.");
+        Matcher matcher = pattern.matcher(str);
+        while (matcher.find()) {
+            result += str.substring(matcher.start(), matcher.end());
+        }
+
+        pattern = Pattern.compile("e");
+        int count = GetCount(pattern, str);
+        if (count > 1) {
+            return null;
+        }
+        pattern = Pattern.compile("\\.");
+        count = GetCount(pattern,str);
+        if (count > 1) {
+            return null;
+        }
+
+        pattern = Pattern.compile("-");
+        count = GetCount(pattern,str);
+        if (count > 1) {
+            if (result.toCharArray()[0] == '-' && result.toCharArray()[1] == '-') {
+                return  null;
+            }
+        }
+
+        try {
+            return NumberFormat.getInstance().parse(result);
+        } catch (Exception e) {
+            return  null;
+        }
     }
 }
+
