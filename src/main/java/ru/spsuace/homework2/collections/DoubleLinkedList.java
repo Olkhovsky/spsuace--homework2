31767d4318954def5bb754fa0f2c637b113e03f4
Olkhovsky <olkhovsky11a@mail.ru>
2019-10-29 13:51:56 +0300

    DoubleLinkedList без доп. задания

--- src/main/java/ru/spsuace/homework2/collections/DoubleLinkedList.java
+++ src/main/java/ru/spsuace/homework2/collections/DoubleLinkedList.java
@@ -8,48 +8,132 @@ import java.util.Iterator;
  * Если в метод передается индекс, которого не существует (за исключением, add(size(), obj)), то надо бросить ошибку:
  * throw new IndexOutOfBoundsException()
  */
+
 public class DoubleLinkedList<T> implements Iterable<T> {
 
     public int size() {
-        return 0;
+        return size;
     }
 
     public boolean contains(Object o) {
-        return false;
+        Link current = first;
+        while(current != null){
+            if (o.equals(current.data)) {
+                return true;
+            }
+            current = current.next;
+        }
+        return  false;
     }
 
     public void clear() {
-
+        last = null;
+        first = null;
+        size = 0;
     }
 
     public void add(int index, T element) {
+        if (index <= 0) {
+            addFirst(element);
+            return;
+        }
 
+        if (index >= size()) {
+            addLast(element);
+            return;
+        }
+
+
+        Link current = getLink(index);
+        size++;
+        Link<T> newLink = new Link<T>(element);
+
+        newLink.next = current;
+        newLink.previous = current.previous;
+        current.previous.next = newLink;
+        current.previous = newLink;
     }
 
     public void addLast(T element) {
+        size++;
+        Link<T> newLink = new Link<T>(element);
+        if (last != null) {
+            newLink.previous = last;
+            last.next = newLink;
+            last = newLink;
+        } else {
+            last = newLink;
+            first = newLink;
+        }
 
     }
 
 
     public void addFirst(T element) {
-
+        size++;
+        Link<T> newLink = new Link<T>(element);
+        if (first != null) {
+            newLink.next = first;
+            first.previous = newLink;
+            first = newLink;
+        } else {
+            last = newLink;
+            first = newLink;
+        }
     }
 
 
     public T set(int index, T element) {
-        return null;
+        if (index >= size || index < 0) {
+            return  null;
+        }
+        Link<T> current = getLink(index);
+        T buffer = current.data;
+        current.data = element;
+        return buffer;
     }
 
     public T get(int index) {
-        return null;
+        if (index >= size || index < 0) {
+            return  null;
+        }
+        return (T)getLink(index).data;
     }
 
     public int indexOf(T o) {
+        int index = -1;
+        Link current = first;
+        while (current != null) {
+            index++;
+            if(current.data.equals(o)) {
+                return index;
+            }
+            current = current.next;
+        }
         return -1;
     }
 
     public T remove(int index) {
-        return null;
+        if (index >= size || index < 0) {
+            return  null;
+        }
+
+        Link current = getLink(index);
+        size--;
+
+        if (index == 0) {
+            first = current.next;
+            return (T)current.data;
+        }
+
+        if (index >= size) {
+            last = current.previous;
+            return (T)current.data;
+        }
+
+        current.next.previous = current.previous;
+        current.previous.next = current.next;
+        return (T)current.data;
     }
 
     /**
@@ -59,4 +143,36 @@ public class DoubleLinkedList<T> implements Iterable<T> {
     public Iterator<T> iterator() {
         return null;
     }
+
+    private Link<T> getLink(int index)
+    {
+        Link current = null;
+        if (index < size / 2) {
+            current = first;
+            for (int i = 0; i < index; i++) {
+                current = current.next;
+            }
+        } else {
+            current = last;
+            for (int i = 0; i < (size -1) - index; i++) {
+                current = current.previous;
+            }
+        }
+        return  current;
+    }
+
+    private static class Link<T> {
+        private T data;
+        private Link next;
+        private Link previous;
+
+        private Link(T data)
+        {
+            this.data = data;
+        }
+    }
+
+    private Link first;
+    private Link last;
+    private int size;
 }
